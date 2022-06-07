498e4a581362ab94b3b2b14390e106e21587620e
kazukisasajima <hoppkazuki@gmail.com>
2022-06-07 00:30:55 +0000

    update layout

---
+++ app/helpers/records_helper.rb
@@ -0,0 +1,8 @@
+module RecordsHelper
+  def text_placeholder 
+    <<-"EOS".strip_heredoc
+午前:　　
+午後:　　　　　　　　
+    EOS
+  end
+end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,26 @@ module UsersHelper
     size = options[:size]
     "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   end
+  
+  
+  def best_placeholder 
+    <<-"EOS".strip_heredoc
+フルマラソン: 3時間00分00秒　　
+5000m: 20'00"00　　　　　　　　
+    EOS
+  end
+  
+  def race_placeholder 
+    <<-"EOS".strip_heredoc
+東京マラソン　　
+〇月〇日 〇〇記録会:5000m　　　　　　　　
+    EOS
+  end
+  
+  def target_placeholder 
+    <<-"EOS".strip_heredoc
+フルマラソン: 3時間00分00秒　　
+5000m: 20'00"00　　　　　　　　
+    EOS
+  end
 end
